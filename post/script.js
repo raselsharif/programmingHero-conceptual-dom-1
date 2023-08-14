39696d6e20ef89a9bddc933118012acc2715d06b
Rasel Sharif <raselsharif9@gmail.com>
2023-08-14 16:22:44 +0600

    added post validation

--- post/script.js
+++ post/script.js
@@ -1,14 +1,21 @@
 
 document.getElementById('post-btn').addEventListener('click', function () {
     const postMsg = document.querySelector("#post-msg");
+    const errorMsg = document.querySelector('#error-msg');
 
-    const postContainer = document.querySelector('#post-section');
+    if (postMsg.value === '') {
+        errorMsg.innerText = "Pls! write something in review area.";
+    } else {
+        const postContainer = document.querySelector('#post-section');
 
-    const p = document.createElement('p');
-    p.classList.add('post')
-    p.innerText = postMsg.value;
+        const p = document.createElement('p');
+        p.classList.add('post')
+        p.innerText = postMsg.value;
 
-    postContainer.appendChild(p);
-    postMsg.value = '';
+        postContainer.append(p);
+        postMsg.value = '';
+        errorMsg.innerText = "";
+
+    }
 
 })
